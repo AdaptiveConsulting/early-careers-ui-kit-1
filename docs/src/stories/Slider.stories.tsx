46c5745f3fc22b532a790c202747eb44a9479df6
Abdurrahman Mohamoud <abdurrahman.mohamoud@weareadaptive.com>
2023-01-19 17:36:19 +0000

    render initial slider component

---
+++ docs/src/stories/Slider.stories.tsx
@@ -0,0 +1,11 @@
+import Slider from "components/src/Slider"
+import { Meta } from "@storybook/react"
+
+const meta: Meta = {
+  title: "Slider",
+  component: Slider,
+}
+
+export default meta
+
+export const Default = () => <Slider />
