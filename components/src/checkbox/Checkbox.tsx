44dd829607fd422caf0686379c8f2a24a93eb487
adrian.rosipal <adrian.rosipal@adrianrosipals-MacBook-Pro.local>
2023-01-23 09:25:50 +0100

    yarn re-install

--- components/src/checkbox/Checkbox.tsx
+++ components/src/checkbox/Checkbox.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 import "./checkbox.css"
-import { useEffect } from "react"
-import { isDepsOptimizerEnabled } from "vite"
 interface CheckboxProps {
   size?: "small" | "medium"
   checkboxType: string
