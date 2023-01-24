38853385f04038cd77357a33e2b41475fac5edd6
adamshcao <adamshcao@gmail.com>
2023-01-24 15:27:00 +0000

    setup: dark light install

--- docs/.storybook/preview.ts
+++ docs/.storybook/preview.ts
@@ -1,3 +1,16 @@
+import { themes } from "@storybook/theming"
+
+export const parameters = {
+  darkMode: {
+    // Set the initial theme
+    current: "light",
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: "black" },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: "white" },
+  },
+}
+
 export default {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
