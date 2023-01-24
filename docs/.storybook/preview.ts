7dcbb21e268c0ca30c98ea6de1a02630c579f563
adamshcao <adamshcao@gmail.com>
2023-01-24 15:30:25 +0000

    setup: theming colours

--- docs/.storybook/preview.ts
+++ docs/.storybook/preview.ts
@@ -2,10 +2,13 @@ import { themes } from "@storybook/theming"
 
 export const parameters = {
   darkMode: {
+    darkClass: "lights-out",
+    lightClass: "lights-on",
+    stylePreview: true,
     // Set the initial theme
     current: "light",
     // Override the default dark theme
-    dark: { ...themes.dark, appBg: "black" },
+    dark: { ...themes.dark, appBg: "#292C2E" },
     // Override the default light theme
     light: { ...themes.normal, appBg: "white" },
   },
